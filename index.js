9b772d4f4d3cd1ec32e3b142da6357bb5c262fb9
Bernadetta Wąsowska <bernadetta.wasowska@gmail.com>
2018-07-18 09:21:15 +0200

    Create first server

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+var http = require('http');
 var fs = require('fs');
 var colors = require('colors');
 var StatMode = require('stat-mode');
@@ -8,7 +9,24 @@ fs.stat('./cat.jpg', function(err, stats){
 	console.log(statMode.toString());
 })
 */
+var server = http.createServer();
+server.on('request', function(request, response) {
+	response.setHeader("Content-Type", "text/html; charset=utf-8");
+	if(request.method==='GET' && request.url==='/hello') {
+		fs.readFile('index.html', 'utf-8', function(err,data) {
+			response.write(data);
+			response.end();
+		});
+	} else {
+		response.statusCode =  404;
+		response.write('<img src="https://i.kym-cdn.com/photos/images/original/000/178/254/c86.jpg" alt="404">');
+		response.end();
+	}
+	
+});
+server.listen(9000);
 
+/*
 fs.readdir('.', function(err, files) {
 	var str = "";
 	files.forEach(function(item) {
@@ -23,3 +41,4 @@ fs.readdir('.', function(err, files) {
 		});
 	});
 }); 
+*/
